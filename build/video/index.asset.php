c96fdb9cf6201a0fe47d20ec0ed1e6fbd7121f91
Aitor Méndez <aitor@e451.net>
2025-04-15 13:16:13 +0200

    Video Block: Corrección CSS editor

--- build/video/index.asset.php
+++ build/video/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cc1aa0cd70e81b6f433a');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '89cf9fe2418cd65693a2');
